f77ae36765f754fdb1b900fbdd6a7d16b3f3765a
yerimlee <144992260+limye2@users.noreply.github.com>
2025-09-25 15:53:58 +0900

    실습4

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  04 실습2
+//  04 실습4
 //
 //  Created by 이예림 on 9/25/25.
 //
@@ -9,12 +9,12 @@
 
 int main(int argc, const char * argv[]) {
     
-    int x;
+    int year;
     
-    printf("input the second : ");
-    scanf("%i", &x);
+    printf("input the year : ");
+    scanf("%i", &year);
     
-    printf("the time is %i : %i\n", x/60, x%60);
+    printf("is the year %i the leap year? : %i\n", year, (year%4==0)&&(year%100!=0)||(year%400==0));
         
     return 0;
 }
